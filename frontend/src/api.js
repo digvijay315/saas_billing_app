3035459cd67568bca862b807018e1f9918695f86
digvijay315 <digvijaykumar.315@gmail.com>
2026-07-24 20:07:39 +0530

    update

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: "http://localhost:5000/",
-  baseURL:"https://hotelshankarpalace.onrender.com/"
+  baseURL:"https://saas-billing-app.onrender.com/"
 });
 
 let activeRequests = 0;
